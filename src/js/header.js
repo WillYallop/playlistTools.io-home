d032225f584ea0607ad84e9cf722a8678e2d0a23
William Yallop <hello@williamyallop.com>
2021-06-27 13:30:31 +0100

    Finish desktop style for header component

--- src/js/header.js
+++ src/js/header.js
@@ -8,11 +8,25 @@ function handleNavDropdown() {
             toolDropdownEle.classList.remove('closed');
             toolDropdownEle.style.maxHeight = `${offsetHeight}px`;
             btn.attributes['aria-expanded'].value = true;
+            // Loop over links and update tabindex
+            let linkElement = document.getElementsByClassName('setTabIndex');
+            if(linkElement != undefined) {
+                for(let i = 0; i < linkElement.length; i++) {
+                    linkElement[i].attributes['tabindex'].value = '0';
+                }
+            }
         } else {
             // Close menu
             toolDropdownEle.classList.add('closed');
             toolDropdownEle.style.maxHeight = `0px`;
             btn.attributes['aria-expanded'].value = false;
+            // Loop over links and update tabindex
+            let linkElement = document.getElementsByClassName('setTabIndex');
+            if(linkElement != undefined) {
+                for(let i = 0; i < linkElement.length; i++) {
+                    linkElement[i].attributes['tabindex'].value = '-1';
+                }
+            }
         }
     })
 }
