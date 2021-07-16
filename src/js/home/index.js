1f60fdd98b330fa2786cc0de189129ea1302e350
William Yallop <hello@williamyallop.com>
2021-07-16 19:48:57 +0100

    Implemented component style lazy load js

--- src/js/home/index.js
+++
@@ -1,1 +0,0 @@
-import GradientCanvas from './banner/gradientCanvas';
--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import { handleNavDropdown, updateDropdownMaxHeight } from './header';
-
-// Add event listener to dropdown
-handleNavDropdown();
-
-// On resize
-window.addEventListener('resize', () => {
-    updateDropdownMaxHeight();
-})
